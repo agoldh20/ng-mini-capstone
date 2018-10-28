9a814c6128990a0b22aa46fe72e68c1da16cfc37
agoldh20 <a.goldh20@gmail.com>
2018-10-28 18:12:35 -0500

    started namespacing and started controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,29 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def create
+    @movie = Movie.new(title: params[:title],
+                          runtime: params[:runtime],
+                          rating: params[:rating])
+    if @movie.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @movie.errors.full_messages}, status: 418
+    end
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+  end
 end
