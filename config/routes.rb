c87b23dcc5876857476404238ef48f297393750a
agoldh20 <a.goldh20@gmail.com>
2018-10-28 21:02:07 -0500

    routes for movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    
+    get '/movies' => 'movies#index'
+    post '/movies' => 'movies#create'
+    get '/movies/:id' => 'movies#show'
+    patch '/movies/:id' => 'movies#update'
+    get '/movies/:id' => 'movies#destroy'
   end
 end
